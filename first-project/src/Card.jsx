b796b98d3881c748ee4b6d36dcd92dbaae69269f
baljinder <baljindermaan15@gmail.com>
2024-10-20 14:09:29 -0400

    card and design

---
+++ first-project/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card (){
+
+    return(
+        <div className="card">
+        <img className="card-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5SQntNi68ASSiKFpRVzQOMfJCx5aYevH69w&s" alt="Profile picture"></img>
+        <h2 className="card-title">Baljinder</h2>
+        <p className="card-text">I make apps and now started websites as well</p>
+        </div>
+    );
+}
+export default Card
