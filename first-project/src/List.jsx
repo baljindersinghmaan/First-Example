fc449909a280482d8d9d65bba8af3c751bb6a1b2
baljinder <baljindermaan15@gmail.com>
2024-10-20 17:58:48 -0400

    list with function

--- first-project/src/List.jsx
+++ first-project/src/List.jsx
@@ -1,11 +1,24 @@
+import PropTypes from 'prop-types'
+
 function List(){
 
-    const fruits = ["Apple", "Bannna","coconut"]
+    const fruits = [ {name: "Bannna", cal: 23},{name: "Apple", cal:45},{name:"coconut", cal:38}]
 
-    const fruitsItem = fruits.map(fruit => <li>{fruit}</li>)
+    //fruits.sort((a, b) => a.name.localeCompare(b.name));
+
+    fruits.sort((a, b) => b.name.localeCompare(a.name));
+
+    const lowCal = fruits.filter(fruit => fruit.cal < 30)
+
+    const fruitsItem = fruits.map(fruit => <li key={fruit.name}>{fruit.name}
+    :&nbsp; <b>{fruit.cal}</b></li>)
 
     return(<ul>{fruitsItem}</ul>);
 
+}
+List.PropTypes = {
+    // this is way of setting prop types of object
+    // fruits: PropTypes.arrayOf(PropTypes.shape({name: PropTypes.string, cal: PropTypes.number}))
 }
 
 export default List
