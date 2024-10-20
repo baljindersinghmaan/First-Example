70589e7b33874cd7069c2c8a536ac8c29be2f2c0
baljinder <baljindermaan15@gmail.com>
2024-10-20 13:45:02 -0400

    Food component

---
+++ first-project/src/Food.jsx
@@ -0,0 +1,12 @@
+function Food() {
+    var food1 = "Apple"
+    var food2 = "Orange"
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+
+export default Food
